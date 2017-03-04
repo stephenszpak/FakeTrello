a33f371d5015ba2204cf6efec4870145a22b60ac
Stephen Szpak <srszpak@gmail.com>
2017-03-04 12:14:38 -0600

    adding DB Context

---
+++ FakeTrello/DAL/FakeTrelloContext.cs
@@ -0,0 +1,17 @@
+﻿using FakeTrello.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FakeTrello.DAL
+{
+    public class FakeTrelloContext
+    {
+        public class MyAwesomeContext : ApplicationDbContext
+        {
+            public virtual DbSet<TrelloUser> Users { get; set; }
+        }
+    }
+}
