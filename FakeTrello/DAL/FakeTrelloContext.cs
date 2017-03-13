77311ec4506cffa1b074f347a646802b784cc3f5
Stephen Szpak <srszpak@gmail.com>
2017-03-13 18:30:32 -0500

    add FakeTrelloReposity and inherited from IRepository

--- FakeTrello/DAL/FakeTrelloContext.cs
+++ FakeTrello/DAL/FakeTrelloContext.cs
@@ -1,12 +1,16 @@
 ﻿using FakeTrello.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace FakeTrello.DAL
 {
     public class FakeTrelloContext : ApplicationDbContext
-    {
+    { 
+        public virtual DbSet<Board> Boards { get; set; }
+        public virtual DbSet<List> Lists { get; set; }
+        public virtual DbSet<Card> Cards { get; set; }
     }
 }
